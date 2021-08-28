803f8729c2eb3aae606ee81a066d40b6d208b94a
Prathmesh Pathak <34978373+prathmesh-pathak@users.noreply.github.com>
2021-08-28 15:59:06 -0400

    created app.js file

---
+++ app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "cAIzaSyBnSrCl0UvzIq1yrDMJ3zsdummy",
+    authDomain: "asdfadsf-a56e7.firebaseapp.com",
+    databaseURL: "httpss://asdfadsf-a56e7.firebaseio.com",
+    projectId: "asdfadsf-a56e7",
+    storageBucket: "asdfadsf-a56e7.appspot.com",
+    messagingSenderId: "104313484945"
+  };
+  firebase.initializeApp(config);
